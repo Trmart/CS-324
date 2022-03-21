19ba03e7f53db6e06d270087c0cefa1d45cfbf5b
Taylor Martin <57766633+Trmart@users.noreply.github.com>
2022-03-21 14:46:53 -0700

    Update canvas.cpp

--- PA 1 (2D plotting) Ex/canvas.cpp
+++ PA 1 (2D plotting) Ex/canvas.cpp
@@ -14,17 +14,21 @@ Canvas::Canvas( std::size_t w, std::size_t h, color bg )
       background(bg) { }
 
 
-void Canvas::SetColor( std::size_t x, std::size_t y, color c ) {
-    if ( x >= 0 && x < width && y >= 0 && y < height ) {
+void Canvas::SetColor( std::size_t x, std::size_t y, color c ) 
+{
+    if ( x >= 0 && x < width && y >= 0 && y < height ) 
+    {
         pixels[y * width + x] = {c};
     }
 }
 
-pixel Canvas::GetPixel( std::size_t x, std::size_t y ) const {
+pixel Canvas::GetPixel( std::size_t x, std::size_t y ) const 
+{
     return pixels[y * width + x];
 }
 
-void Canvas::SetPixel( std::size_t x, std::size_t y, pixel p ) {
+void Canvas::SetPixel( std::size_t x, std::size_t y, pixel p ) 
+{
 
     if( x >= 0 && x < width && y >= 0 && y < height )
     {
@@ -32,8 +36,10 @@ void Canvas::SetPixel( std::size_t x, std::size_t y, pixel p ) {
     }
 }
 
-void Canvas::Clear() {
-    for( auto& pixel : pixels ) {
+void Canvas::Clear() 
+{
+    for( auto& pixel : pixels ) 
+    {
         pixel = {background};
     }
 }
@@ -43,25 +49,33 @@ void Line( Canvas& c, int x1, int y1, int x2, int y2, color color )
 {
     int xTmp, yTmp;
 
-    if( y1 == y2 ) {          // Horizontal
-        if( x1 > x2 ) {  // order for loop to work
+    if( y1 == y2 ) 
+    {          // Horizontal
+        if( x1 > x2 ) 
+	{  // order for loop to work
 	    xTmp = x1;
 	    x1 = x2;
 	    x2 = xTmp;
 	}
 	    
-        for( auto i = x1 ; i <= x2 ; ++i ) {
+        for( auto i = x1 ; i <= x2 ; ++i ) 
+	{
             c.SetColor( i, y1, color );
         }
         return;
-    } else if ( x1 == x2 ) {   // Vertical
-        if( y1 > y2 ) {  // order for loop to work
+    } 
+	
+    else if ( x1 == x2 ) 
+    {   // Vertical
+        if( y1 > y2 ) 
+	{  // order for loop to work
 	    yTmp = y1;
 	    y1 = y2;
 	    y2 = yTmp;
 	}
 	    
-        for( auto j = y1 ; j <= y2 ; ++j ) {
+        for( auto j = y1 ; j <= y2 ; ++j ) 
+	{
             c.SetColor( x1, j, color );
         }
         return;
@@ -69,20 +83,32 @@ void Line( Canvas& c, int x1, int y1, int x2, int y2, color color )
 
     double a = 1.0 * (y1 - y2) / (x1 - x2); //formula for slope
     double b = y1 - a * x1; // y-intercept
-    for( double i = std::min(x1, x2) ; i < std::max(x1, x2) ; i += 1.0 ) {
-        for( double j = std::min(y1, y2) ; j < std::max(y1, y2) ; j += 1.0 ) {
+    for( double i = std::min(x1, x2) ; i < std::max(x1, x2) ; i += 1.0 ) 
+    {
+        for( double j = std::min(y1, y2) ; j < std::max(y1, y2) ; j += 1.0 ) 
+	{
             double x1_intercept = a * (i - 0.5) + b;
             double x2_intercept = a * (i + 0.5) + b;
             double y1_intercept = (j - b - 0.5) / a;
             double y2_intercept = (j - b + 0.5) / a;
             
-            if (x1_intercept >= j - 0.5 && x1_intercept <= j + 0.5) {
+            if (x1_intercept >= j - 0.5 && x1_intercept <= j + 0.5) 
+	    {
                 c.SetColor(i, j, color);
-            } else if (x2_intercept >= j - 0.5 && x2_intercept <= j + 0.5) {
+            } 
+		
+	    else if (x2_intercept >= j - 0.5 && x2_intercept <= j + 0.5) 
+	    {
                 c.SetColor(i, j, color);
-            } else if (y1_intercept >= i - 0.5 && y1_intercept <= i + 0.5) {
+            } 
+		
+	    else if (y1_intercept >= i - 0.5 && y1_intercept <= i + 0.5) 
+	    {
                 c.SetColor(i, j, color);
-            } else if (y2_intercept >= i - 0.5 && y2_intercept <= i + 0.5) {
+            } 
+		
+	    else if (y2_intercept >= i - 0.5 && y2_intercept <= i + 0.5) 
+	    {
                 c.SetColor(i, j, color);
             }
         }
@@ -96,8 +122,10 @@ void SaveCanvasToFile( Canvas const& canvas, std::string const& fileName )
     
     file << "P6 " << canvas.Width() << " " << canvas.Height() << " 255\n";
 
-    for( auto y = 0 ; y < canvas.Height() ; ++y ) {
-        for( auto x = 0 ; x < canvas.Width() ; ++x ) {
+    for( auto y = 0 ; y < canvas.Height() ; ++y ) 
+    {
+        for( auto x = 0 ; x < canvas.Width() ; ++x )
+	{
             auto const pixel = canvas.GetPixel(x, y);
             file << pixel.c.red << pixel.c.green << pixel.c.blue;
         }
